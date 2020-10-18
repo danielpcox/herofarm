dbb8e13e09c0313d3576bfc4d8bf5c3ad0b63b64
Daniel Cox <danielpcox@gmail.com>
2020-10-17 20:31:37 -0400

    player motion and animations

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector2 change;
+    public float speed;
+    private Animator animator;
+
+    private Rigidbody2D physicsBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        physicsBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+
+        if (change != Vector2.zero)
+        {
+            Vector2 position = physicsBody.position;
+            position += change * speed * Time.deltaTime;
+            physicsBody.MovePosition(position);
+            animator.SetFloat("MoveX", change.x);
+            animator.SetFloat("MoveY", change.y);
+            animator.SetBool("Moving", true);
+        }
+        else
+        {
+            animator.SetBool("Moving", false);
+        }
+    }
+}
