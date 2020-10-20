a24f19fbb2dc48245ee6b163a8ccf4a8fb4ff22a
Daniel Cox <danielpcox@gmail.com>
2020-10-19 20:17:34 -0400

    pixelperfect and raycast

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,25 +5,55 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Vector2 change;
+    private Vector2 lookDirection;
     public float speed;
     private Animator animator;
 
     private Rigidbody2D physicsBody;
 
-    // Start is called before the first frame update
+    private ArrayList inventory = new ArrayList();
+
     void Start()
     {
         animator = GetComponent<Animator>();
         physicsBody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
         change.Normalize();
-        
+        if (change != Vector2.zero)
+        {
+            lookDirection = change;
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(
+                physicsBody.position + Vector2.up * 0.2f,
+                lookDirection,
+                1.5f,
+                LayerMask.GetMask("Collectibles"));
+            if (hit.collider != null)
+            {
+                GameObject go = hit.collider.gameObject;
+                Debug.Log(go.name);
+                inventory.Add(go);
+                go.SetActive(false);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (inventory.Count > 0)
+            {
+                GameObject go = (GameObject)inventory[0];
+                go.SetActive(true);
+                inventory.RemoveAt(0);
+            }
+        }
     }
 
     private void FixedUpdate()
