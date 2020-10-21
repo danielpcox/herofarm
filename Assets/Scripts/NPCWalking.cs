ce797408199969a6f6010736a6b272afa3857512
Daniel Cox <danielpcox@gmail.com>
2020-10-20 23:28:44 -0400

    doughboy townsperson walks to the right and disappears

---
+++ Assets/Scripts/NPCWalking.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCWalking : MonoBehaviour
+{
+    public float speed;
+    public int disappearAfterSeconds;
+
+    private Vector2 position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DisappearAfterAwhile());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        position = transform.position;
+        position.x += speed * Time.deltaTime;
+        transform.position = position;
+    }
+
+    IEnumerator DisappearAfterAwhile()
+    {
+        yield return new WaitForSeconds(disappearAfterSeconds);
+        Destroy(gameObject);
+    }
+}
