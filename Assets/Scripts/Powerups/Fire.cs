ea91b84d873bb290a4e53530506f9123aa4c207a
Daniel Cox <danielpcox@gmail.com>
2020-11-07 16:26:43 -0500

    Fire that reduces your energy to touch

---
+++ Assets/Scripts/Powerups/Fire.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerController player = collision.GetComponent<PlayerController>();
+        if (player != null)
+        {
+            player.ChangeEnergy(-1);
+        }
+    }
+}
