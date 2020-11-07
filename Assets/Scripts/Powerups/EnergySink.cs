bbdff5a1b32cd3e1f6c684f7c6d672b0d6695299
Daniel Cox <danielpcox@gmail.com>
2020-11-07 17:02:48 -0500

    added cave tileset and EnergySinks tilemap

--- Assets/Scripts/Powerups/EnergySink.cs
+++ Assets/Scripts/Powerups/EnergySink.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Fire : MonoBehaviour
+public class EnergySink : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
